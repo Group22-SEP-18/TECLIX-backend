d4c796c5391b7332fc48aea0836dae153cecbe0d
binoy <binoy.18@cse.mrt.ac.lk>
2021-09-18 08:41:25 +0530

    bug fix

--- customer_api/serializers.py
+++ customer_api/serializers.py
@@ -5,7 +5,8 @@ from .models import Customer
 class CustomerViewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
-        fields = '__all__'
+        # fields = '__all__'
+        exclude = ['created_by']
 
     def validate(self, attrs):
         contact_no = attrs.get('contact_no', '')
@@ -13,4 +14,3 @@ class CustomerViewSerializer(serializers.ModelSerializer):
         if len(contact_no) < 10:
             raise serializers.ValidationError('The contact number is invalid')
         return attrs
-    
