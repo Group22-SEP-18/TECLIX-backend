be63f838b24efea785802e9b51d28baa77d84b93
binoy <binoy.18@cse.mrt.ac.lk>
2021-09-18 11:36:02 +0530

    product model done

--- asset_api/models.py
+++ asset_api/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+
+class Product(models.Model):
+    CATEGORY_OPTIONS = {
+        ('biscuit', 'Biscuit'),
+        ('cake', 'Cake'),
+    }
+    short_name = models.CharField(max_length=80)
+    long_name = models.CharField(max_length=250)
+    product_image = models.ImageField(upload_to='product/', max_length=255)
+    category = models.CharField(choices=CATEGORY_OPTIONS, max_length=150)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+
+    def __str__(self):
+        return self.long_name
