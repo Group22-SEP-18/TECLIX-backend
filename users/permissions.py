db14ad8e7bffea4732f850edd9b1cf22da9860cf
binoy <binoy.18@cse.mrt.ac.lk>
2021-09-20 00:08:33 +0530

    base permission bug fix

--- users/permissions.py
+++ users/permissions.py
@@ -2,20 +2,25 @@ from rest_framework import permissions
 
 
 class IsSalesperson(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-        print(request.user.user_role)
+    def has_permission(self, request, view):
+        return request.user.user_role == 'SALESPERSON'
 
+    def has_object_permission(self, request, view, obj):
         return request.user.user_role == 'SALESPERSON'
 
 
 class IsManager(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-        print(request.user.user_role)
 
+    def has_permission(self, request, view):
+        return request.user.user_role == 'MANAGER'
+
+    def has_object_permission(self, request, view, obj):
         return request.user.user_role == 'MANAGER'
 
 
 class IsOfficer(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.user_role == 'OFFICER'
+
     def has_object_permission(self, request, view, obj):
-        print(request.user.user_role)
         return request.user.user_role == 'OFFICER'
