62320d21d234681ef83f148a871cefb539c4f5fc
binoy <binoy.18@cse.mrt.ac.lk>
2021-09-19 19:22:48 +0530

    approve acc endpoints done

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from knox import views as knox_views
-from .views import RegisterStaffView, LoginStaffView, GetLoggedUserFromToken
+from .views import RegisterStaffView, LoginStaffView, GetLoggedUserFromToken, UpdateSalespersonAccStateView, \
+    UpdateDistOfficerAccStateView
 
 urlpatterns = [
     path('register/', RegisterStaffView.as_view(), name='staff-register'),
     path('login/', LoginStaffView.as_view(), name='staff-login'),
     path('logged-in-user/', GetLoggedUserFromToken.as_view(), name='staff-logged-user'),
     path('logout/', knox_views.LogoutView.as_view(), name='staff-logout'),
+    path('approve/salesperson/<int:id>', UpdateSalespersonAccStateView.as_view(), name='approve-sp'),
+    path('approve/distribution-officer/<int:id>', UpdateDistOfficerAccStateView.as_view(), name='approve-do'),
+
 ]
