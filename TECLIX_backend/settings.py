7c385f9580a1a77aa0588f790a903e5e9395dc7e
binoy <binoy.18@cse.mrt.ac.lk>
2021-09-20 10:53:50 +0530

    deployment code

--- TECLIX_backend/settings.py
+++ TECLIX_backend/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 from decouple import config
 import dj_database_url
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -152,3 +153,4 @@ CLOUDINARY_STORAGE = {
     "API_SECRET": config('API_SECRET')
 
 }
+django_heroku.settings(locals())
