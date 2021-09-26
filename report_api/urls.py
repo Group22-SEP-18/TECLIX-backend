ad00d2e48145ccdafe3a8fe254cab136af7c4ae5
binoy <binoy.18@cse.mrt.ac.lk>
2021-09-26 08:11:38 +0530

    get report last 2 mon n sp

--- report_api/urls.py
+++ report_api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import GetMonthlySalespersonSalesView
+from .views import GetMonthlySalespersonSalesView, GetTwoMonthComparisonSalesView
 
 urlpatterns = [
     path('salesperson/month/<int:sp>', GetMonthlySalespersonSalesView.as_view(), name='salesperson-month'),
+    path('salesperson/month-comparison/<int:sp>', GetTwoMonthComparisonSalesView.as_view(),
+         name='salesperson-two-month'),
 
 ]
--- report_api/views.py
+++ report_api/views.py
@@ -15,3 +15,28 @@ class GetMonthlySalespersonSalesView(generics.GenericAPIView):
         for item in so:
             total_sales += item.original_price
         return Response({'salesperson': sp, 'total_sales': total_sales}, status=status.HTTP_200_OK)
+
+
+class GetTwoMonthComparisonSalesView(generics.GenericAPIView):
+
+    def get(self, request, sp):
+        if datetime.date.today().month == 1:
+            prev_month = 12
+            prev_month_year = datetime.date.today().year - 1
+        else:
+            prev_month = datetime.date.today().month - 1
+            prev_month_year = datetime.date.today().year
+        so = ServiceOrder.objects.filter(order_date__month=datetime.date.today().month, salesperson=sp,
+                                         order_date__year=datetime.date.today().year)
+        prev_so = ServiceOrder.objects.filter(order_date__month=prev_month, salesperson=sp,
+                                              order_date__year=prev_month_year)
+        cur_sales = 0
+        prev_sales = 0
+
+        for i in so:
+            cur_sales += i.original_price
+
+        for j in prev_so:
+            prev_sales += j.original_price
+        return Response({'salesperson': sp, 'sales_current_month': cur_sales, 'sales_last_month': prev_sales},
+                        status=status.HTTP_200_OK)
