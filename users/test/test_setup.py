f890151ca3ffed29e0a60365a56d0ddb32196c04
binoy <binoy.18@cse.mrt.ac.lk>
2021-10-08 18:23:04 +0530

    login views done

--- users/test/test_setup.py
+++ users/test/test_setup.py
@@ -21,11 +21,12 @@ class TestSetUp(APITestCase):
         self.register_url = reverse('staff-register')
         self.mobile_login_url = reverse('mobile-login')
         self.web_login_url = reverse('web-login')
+        self.logged_user = reverse('staff-logged-user')
         self.faker = Faker()
 
         # login data
         self.register_salesperson_data = {
-            "email": self.faker.email(),
+            "email": 'test@gmail.com',
             "employee_no": 'testid',
             "password": 'password',
             "user_role": 'SALESPERSON',
@@ -35,7 +36,7 @@ class TestSetUp(APITestCase):
             "profile_picture": generate_photo_file(),
         }
         self.register_manager_data = {
-            "email": self.faker.email(),
+            "email": 'test@gmail.com',
             "employee_no": 'testid',
             "password": 'password',
             "user_role": 'MANAGER',
@@ -45,7 +46,7 @@ class TestSetUp(APITestCase):
             "profile_picture": generate_photo_file(),
         }
         self.register_officer_data = {
-            "email": self.faker.email(),
+            "email": 'test@gmail.com',
             "employee_no": 'testid',
             "password": 'password',
             "user_role": 'OFFICER',
@@ -53,6 +54,12 @@ class TestSetUp(APITestCase):
             "last_name": self.faker.last_name(),
             "contact_no": '1234567890',
             "profile_picture": generate_photo_file(),
+        }
+
+        self.login_cred = {
+            "email": 'test@gmail.com',
+            "password": 'password',
+
         }
         return super().setUp()
 
--- users/test/test_views.py
+++ users/test/test_views.py
@@ -5,6 +5,14 @@ from ..models import Staff
 
 
 class TestView(TestSetUp):
+    def approve_account(self, user_data):
+        res1 = self.client.post(self.register_url, user_data, format='multipart')
+        # approving account
+        user_email = email = res1.data['email']
+        user = Staff.objects.get(email=user_email)
+        user.is_approved = True
+        user.save()
+
     # resgister view related
     def test_user_cannot_register_without_data(self):
         res = self.client.post(self.register_url)
@@ -38,10 +46,107 @@ class TestView(TestSetUp):
         self.assertEqual(res.status_code, 400)
 
     def test_user_first_name_should_contain_only_letters(self):
-        self.register_salesperson_data['first_name'] = 'bin2'
+        self.register_salesperson_data['first_name'] = 'name@'
         res = self.client.post(self.register_url, self.register_salesperson_data, format='multipart')
         # pdb.set_trace()
         self.assertEqual(res.data['non_field_errors'][0], 'The first name should only contain letters')
         self.assertEqual(res.status_code, 400)
 
-#
+    # login view
+    def test_user_cannot_login_without_data(self):
+        res = self.client.post(self.mobile_login_url)
+        self.assertEqual(res.status_code, 400)
+
+    def test_mobile_user_cannot_login_without_being_approved(self):
+        user = self.client.post(self.register_url, self.register_salesperson_data, format='multipart')
+        res = self.client.post(self.mobile_login_url, self.login_cred)
+        # pdb.set_trace()
+
+        self.assertEqual(res.data['detail'], 'Your account is not approved.')
+        self.assertEqual(res.status_code, 401)
+
+    def test_web_user_cannot_login_without_being_approved(self):
+        user = self.client.post(self.register_url, self.register_officer_data, format='multipart')
+        res = self.client.post(self.mobile_login_url, self.login_cred)
+        # pdb.set_trace()
+
+        self.assertEqual(res.data['detail'], 'Your account is not approved.')
+        self.assertEqual(res.status_code, 401)
+
+    def test_mobile_user_can_login(self):
+        self.approve_account(user_data=self.register_salesperson_data)
+        res = self.client.post(self.mobile_login_url, self.login_cred)
+        # pdb.set_trace()
+
+        self.assertIsNotNone(res.data['token'])
+        self.assertEqual(res.status_code, 200)
+
+    def test_web_user_can_login(self):
+        self.approve_account(user_data=self.register_manager_data)
+        res = self.client.post(self.web_login_url, self.login_cred)
+        # pdb.set_trace()
+
+        self.assertIsNotNone(res.data['token'])
+        self.assertEqual(res.status_code, 200)
+
+    def test_web_user_can_not_login_to_mobile_app(self):
+        self.approve_account(user_data=self.register_manager_data)
+        res = self.client.post(self.mobile_login_url, self.login_cred)
+        # pdb.set_trace()
+
+        self.assertEqual(res.data['detail'], 'You are not authorized to use the application.')
+        self.assertEqual(res.status_code, 401)
+
+    def test_mobile_user_can_not_login_to_web_app(self):
+        self.approve_account(user_data=self.register_salesperson_data)
+        res = self.client.post(self.web_login_url, self.login_cred)
+        # pdb.set_trace()
+
+        self.assertEqual(res.data['detail'], 'You are not authorized to use the application.')
+        self.assertEqual(res.status_code, 401)
+
+    def test_mobile_user_auto_login_via_token(self):
+        self.approve_account(user_data=self.register_salesperson_data)
+        res = self.client.post(self.mobile_login_url, self.login_cred)
+
+        token = res.data['token']
+        header = {'HTTP_AUTHORIZATION': 'Token ' + token}
+        res = self.client.get(self.logged_user, **header)
+
+        self.assertIsNotNone(res.data['token'])
+        self.assertEqual(res.status_code, 200)
+
+    def test_web_user_auto_login_via_token(self):
+        self.approve_account(user_data=self.register_manager_data)
+        res = self.client.post(self.web_login_url, self.login_cred)
+
+        token = res.data['token']
+        header = {'HTTP_AUTHORIZATION': 'Token ' + token}
+        res = self.client.get(self.logged_user, **header)
+
+        self.assertIsNotNone(res.data['token'])
+        self.assertEqual(res.status_code, 200)
+
+    def test_user_can_not_auto_login_once_logged_out(self):
+        self.approve_account(user_data=self.register_manager_data)
+        res = self.client.post(self.web_login_url, self.login_cred)
+
+        res = self.client.get(self.logged_user)
+
+        # pdb.set_trace()
+        self.assertEqual(res.data['detail'], 'Authentication credentials were not provided.')
+
+        self.assertEqual(res.status_code, 401)
+
+    def test__user_can_not_auto_with_invalid_token(self):
+        self.approve_account(user_data=self.register_manager_data)
+        res = self.client.post(self.web_login_url, self.login_cred)
+
+        token = 'dummy-token'
+        header = {'HTTP_AUTHORIZATION': 'Token ' + token}
+        res = self.client.get(self.logged_user, **header)
+
+        # pdb.set_trace()
+        self.assertEqual(res.data['detail'], 'Invalid token.')
+
+        self.assertEqual(res.status_code, 401)
