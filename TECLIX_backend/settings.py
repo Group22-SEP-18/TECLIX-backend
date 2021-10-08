733014787c916c43a1a8387fd08c1de5bf5dd84a
binoy <binoy.18@cse.mrt.ac.lk>
2021-10-08 17:30:35 +0530

    coverage lib added

--- TECLIX_backend/settings.py
+++ TECLIX_backend/settings.py
@@ -51,6 +51,14 @@ INSTALLED_APPS = [
     'salesperson_api',
     'corsheaders',
     'report_api',
+    'django_nose',
+
+]
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+
+NOSE_ARGS = [
+    '--with-coverage',
+    '--cover-package=users',
 
 ]
 
