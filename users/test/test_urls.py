63b04e3fef8ab6341e89224e2914a892a8657cb7
binoy <binoy.18@cse.mrt.ac.lk>
2021-10-12 21:00:10 +0530

    urls testing done

---
+++ users/test/test_urls.py
@@ -0,0 +1,42 @@
+import pdb
+
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from ..views import RegisterStaffView, LoginWebStaffView, LoginSalespersonStaffView, GetLoggedUserFromToken, \
+    GetDOAccountsView, UpdateSalespersonAccStateView, UpdateDistOfficerAccStateView
+from knox import views as knox_views
+
+
+class TestUrls(SimpleTestCase):
+    
+    def test_staff_register_url(self):
+        url = reverse('staff-register')
+        self.assertEqual(resolve(url).func.view_class, RegisterStaffView)
+
+    def test_web_login_url(self):
+        url = reverse('web-login')
+        self.assertEqual(resolve(url).func.view_class, LoginWebStaffView)
+
+    def test_mobile_login_url(self):
+        url = reverse('mobile-login')
+        self.assertEqual(resolve(url).func.view_class, LoginSalespersonStaffView)
+
+    def test_auto_login_url(self):
+        url = reverse('staff-logged-user')
+        self.assertEqual(resolve(url).func.view_class, GetLoggedUserFromToken)
+
+    def test_user_logout_url(self):
+        url = reverse('staff-logout')
+        self.assertEqual(resolve(url).func.view_class, knox_views.LogoutView)
+
+    def test_get_pending_do_account_url(self):
+        url = reverse('do-accounts')
+        self.assertEqual(resolve(url).func.view_class, GetDOAccountsView)
+
+    def test_approve_sp_account_url(self):
+        url = reverse('approve-sp', kwargs={'id': 1})
+        self.assertEqual(resolve(url).func.view_class, UpdateSalespersonAccStateView)
+
+    def test_approve_do_account_url(self):
+        url = reverse('approve-do', kwargs={'id': 1})
+        self.assertEqual(resolve(url).func.view_class, UpdateDistOfficerAccStateView)
